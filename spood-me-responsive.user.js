9b5c0fff1d557aade60aef5607589caeaf5083ca
Dave <theyve@gmail.com>
2018-05-28 14:13:57 +0200

    insertBefore gwts the job done

--- spood-me-responsive.user.js
+++ spood-me-responsive.user.js
@@ -3,19 +3,19 @@
 // @description A userscript which tries to make spood.me less of a hassle to use on a small screen.
 // @copyright 2018, Dave Spengeler (davespengeler.ch)
 // @icon https://raw.githubusercontent.com/theyve/spood-me-responsive/master/icon-32x32.png
-// @version 0.1.9
+// @version 0.2
 // @license GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @match *://*.spood.me/*
 // @run-at document-idle
 // ==/UserScript==
 
 function addCSS(fileName) {
-  var head = document.head;
+  var body = document.body;
   var link = document.createElement("link");
   link.type = "text/css";
   link.rel = "stylesheet";
   link.href = fileName;
-  head.appendChild(link);
+  body.insertBefore(link, null);
 }
 function addViewportMetatag() {
   var meta = document.createElement('meta');
