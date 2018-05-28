f89463df8acaaccced2a7788bc18ca0fd1017649
Dave <theyve@gmail.com>
2018-05-28 13:08:04 +0200

    deleted icon

--- spood-me-responsive.user.js
+++ spood-me-responsive.user.js
@@ -2,7 +2,6 @@
 // @name spood.me - responsive
 // @description A userscript which tries to make spood.me less of a hassle to use on a small screen.
 // @copyright 2018, Dave Spengeler (davespengeler.ch)
-// @icon https://spood.me/img/site/logo.png
 // @version 0.1.0
 // @license GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @include *.spood.me/*
