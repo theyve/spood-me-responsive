222fec8e264b5027e21b9ab6c7f78b60113de78e
Dave <theyve@gmail.com>
2018-05-28 13:13:15 +0200

    added icon to metablock

--- spood-me-responsive.user.js
+++ spood-me-responsive.user.js
@@ -2,6 +2,7 @@
 // @name spood.me - responsive
 // @description A userscript which tries to make spood.me less of a hassle to use on a small screen.
 // @copyright 2018, Dave Spengeler (davespengeler.ch)
+// @icon https://raw.githubusercontent.com/theyve/spood-me-responsive/master/icon-32x32.png
 // @version 0.1.0
 // @license GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @include *.spood.me/*
